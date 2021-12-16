ea0fe53925e9999bb9999cb640771bbe8ca43311
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-12-16 14:00:20 +0530

    Feat: Private route implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {Routes,Route,Navigate} from "react-router-dom";
+import {Home,Login} from "./Pages/index"
+import { useAuth } from './Context/AuthContext';
 
 function App() {
+
+  const {login} = useAuth()
+
+  function PrivateRoute({path,login,props}){
+    return login?<Route path={path} {...props}/>:<Navigate state={{from:path}} replace to="/login"/>
+  }
+
   return (
     <div className="App">
-      
+      <Routes>
+        <PrivateRoute login={login} path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
