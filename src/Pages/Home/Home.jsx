0b15685e090b3a3c40251ef06cc5e7193092a9ce
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-12-16 15:43:05 +0530

    Feat: Logout added to Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useAuth } from '../../Context/AuthContext'
+import './Home.css'
 
 function Home() {
+    const {logoutUser} = useAuth()
     return (
         <div>
-            Home Page
+            <h1>Hello Admin</h1>
+            <button className='logout-button' onClick={logoutUser}>Log Out</button>
         </div>
     )
 }
